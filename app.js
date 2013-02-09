5b78d82580931effc63240731944d61c74ac9270
Pascal Garber <jumplink@gmail.com>
2013-02-09 01:11:27 +0100

    json-cleanup, default-json-parameter false now, removed json-property

--- app.js
+++ app.js
@@ -34,13 +34,14 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/resource_manager/map_manager', map_manager.default);
 app.get('/resource_manager/map_manager/:filename', map_manager.filename);
+app.get('/resource_manager/map_manager/:filename/:layer_index', map_manager.layer.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/map_manager.js
+++ routes/map_manager.js
@@ -1,81 +1,112 @@
 
 //TODO move to librpg-node
-function parse_map_query(map) {
-	return {
-		with_filename: (map.filename == '' || map.filename == 1),
-		with_orientation: (map.orientation == '' || map.orientation == 1),
-		with_version: (map.version == '' || map.version == 1),
-		with_size: (map.size == '' || map.size == 1),
-		with_tilesize: (map.tilesize == '' || map.tilesize == 1),
-		with_property: (map.property == '' || map.property == 1),
-		with_layer: (map.layer == '' || map.layer == 1),
-		with_merged_layer_pixbuf: (map.pixbuf == '' || map.pixbuf == 1)
-	};
+function parse_map_layer_query (rpg, layer) {
+	if (layer === undefined) {
+		return new rpg.MapLayerJsonParam ();
+	} else {
+		return new rpg.MapLayerJsonParam({
+			with_name : (layer.name == '' || layer.name == 1),
+			with_zoff : (layer.zoff == '' || layer.zoff == 1),
+			with_size : (layer.size == '' || layer.size == 1),
+			with_collision : (layer.collision == '' || layer.collision == 1),
+			with_data : (layer.data == '' || layer.data == 1),
+			with_texture : (layer.texture == '' || layer.texture == 1)
+		});
+	}
 }
+
 //TODO move to librpg-node
-function get_map_query_deault(map) {
-	return {
-		with_filename: true,
-		with_orientation: false,
-		with_version: false,
-		with_size: false,
-		with_tilesize: false,
-		with_property: false,
-		with_layer: false,
-		with_merged_layer_pixbuf: false
-	};
+function parse_map_query (rpg, map) {
+	console.log(map);
+	if (map === undefined) {
+		return new rpg.MapJsonParam ();
+	} else {
+		return new rpg.MapJsonParam ({
+			with_filename : (map.filename == '' || map.filename == 1),
+			with_orientation : (map.orientation == '' || map.orientation == 1),
+			with_version : (map.version == '' || map.version == 1),
+			with_size : (map.size == '' || map.size == 1),
+			with_tilesize : (map.tilesize == '' || map.tilesize == 1),
+			with_property : (map.property == '' || map.property == 1),
+			with_merged_layer_pixbuf : (map.pixbuf == '' || map.pixbuf == 1),
+			map_layer_params : parse_map_layer_query(rpg, map.layer)
+		});
+	}
 }
+
+function parse_map_manager_query (rpg, map_manager) {
+	if (map_manager === undefined) {
+		return new rpg.MapManagerJsonParam ();
+	} else {
+		return new rpg.MapManagerJsonParam ({
+			with_folder : (map_manager.folder == '' || map_manager.folder == 1),
+			map_params : parse_map_query (rpg, map_manager.map)
+		});
+	}
+}
+
 /*
  * GET users listing.
  */
 module.exports = function (rpg, resource_manager) {
 	return {
 		default: function (req, res){
-
 			var param_count = Object.keys(req.query).length;
 
 			if(param_count > 0) {
+				var vala_map_params = parse_map_query(rpg, req.query.map);
 
-
-				var vala_map_params;
-				if(map !== undefined) {
-					vala_map_params = new rpg.MapJsonParam(parse_map_query(req.map));
-				} else {
-					vala_map_params = new rpg.MapJsonParam(get_map_query_deault());
-				}
-
-				var vala_map_manager_params = new rpg.MapManagerJsonParam({
-					with_folder : (req.query.folder == '' || req.query.folder == 1),
-					map_params : vala_map_params
-				});
+				var vala_map_manager_params = parse_map_manager_query (rpg, req.query);
 
 				json = resource_manager.mapmanager.get_json_indi_as_str(vala_map_manager_params);
-			} else
-				json = resource_manager.mapmanager.json_str;
+			} else {
+				json = '{"error": "no query string passed"}';
+			}
 
 			res.setHeader('Content-Type', 'application/json');
 			res.send(json);
 		},
-		filename: function (req, res){
 
+		filename: function (req, res) {
 			var param_count = Object.keys(req.query).length;
 
 			var map = resource_manager.mapmanager.get_from_filename(req.params.filename);
 
-			var vala_map_params = new rpg.MapJsonParam(parse_map_query(req));
+			var vala_map_params = parse_map_query(rpg, req.query);
 
 			if(map != null) {
 				var json;
 				if(param_count > 0)
 					json = map.get_json_indi_as_str(vala_map_params);
 				else
-					json = map.json_str;
+					json = '{"error": "no query string passed"}';
 				res.setHeader('Content-Type', 'application/json');
 				res.send(json);
 			} else {
 				res.json({error:"map not found", filename:req.params.filename});
 			}
-		}
+		},
 
+		layer: {
+			index: function (req, res) {
+				var param_count = Object.keys(req.query).length;
+
+				var layer = resource_manager.mapmanager.get_from_filename(req.params.filename).get_layer_from_index(req.params.layer_index);
+
+				var vala_map_layer_params = parse_map_layer_query(rpg, req.query);
+
+				if(layer != null) {
+					var json;
+					if(param_count > 0)
+						json = layer.get_json_indi_as_str(vala_map_layer_params);
+					else
+						json = '{"error": "no query string passed"}';
+					res.setHeader('Content-Type', 'application/json');
+					res.send(json);
+				} else {
+					res.json({error:"layer not found", filename:req.params.filename});
+				}
+			}
+		}
 	};
 };
